f7738ef886f50dc2eb0e6671b21e6edefca6972d
Vinit Kumar <vineetpuniya392@gmail.com>
2025-11-17 13:15:01 +0530

    classwork done

---
+++ ClassWork/classwork2-ques1.js
@@ -0,0 +1,5 @@
+console.log(Number("50"), Boolean("50"), String("50"));
+console.log(Number("hello"), Boolean("hello"), String("hello"));
+console.log(Number(false), Boolean(false), String(false));
+console.log(Number(null), Boolean(null), String(null));
+console.log(Number("100px"), Boolean("100px"), String("100px"));
---
+++ ClassWork/classwork2-ques3.js
@@ -0,0 +1,4 @@
+let str = "45.67";
+let num = parseFloat(str);
+console.log(num);
+console.log(isNaN(num));
